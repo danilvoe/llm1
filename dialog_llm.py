9303b200e45afcd0bf0f997df508365ab10c9f5c
voe <voe@local.ru>
2025-08-11 20:58:27 +0300

    day2

--- dialog_llm.py
+++ dialog_llm.py
@@ -1,9 +1,15 @@
 import requests
+import ollama
+import json 
 
 OLLAMA_URL = "http://localhost:11434"
 MAX_MESSAGES = 10
+SYSTEM_PROMPT = "Твоя задача - давать точные, технически корректные ответы на вопросы о языке программирования Python. Важно: 1. Ответ должен содержать пояснения Python. 2. Все ответы должны быть строго в формате JSON с обязательными полями: 'answer' (основной ответ), 'code' (пример кода если требуется), 'note' (дополнительные пояснения если нужны). 3. Если вопрос не относится к Python или ответ неизвестен, верни JSON с полем 'error': 'Не знаю'. 4. Не добавляй никаких пояснений вне структуры JSON. 5. Ты не должен генерировать отсутсвующие в схеме элементы. 6. После генерации ответа проветь корректность ответа", 
 
-def get_llm_response(prompt, conversation_history=None):
+
+MODEL = "qwen3-coder:30b"
+
+def get_llm_response_requests(prompt, conversation_history=None):
     try:
         url = f"{OLLAMA_URL}/api/generate"
         if conversation_history:
@@ -12,14 +18,14 @@ def get_llm_response(prompt, conversation_history=None):
         else:
             full_prompt = prompt
         payload = {
-            "model": "qwen3-coder:30b",
+            "model": MODEL,
             "prompt": full_prompt,
             "stream": False
         }
         response = requests.post(url, json=payload, timeout=30)
         if response.status_code == 200:
             data = response.json()
-            return data.get("response", "Нет ответа от модели")
+            return data.get("response", "{}")
         else:
             return f"Ошибка API: {response.status_code} - {response.text}"
     except Exception as e:
@@ -53,7 +59,8 @@ def get_ollama_info():
         }
 
 def main():
-    print("Простой чат-бот")
+    print("Простой чат-бот для помощи в Python!!!")
+    print("+" * 40)
     print("Введите '!quit' для выхода")
     print("Введите '!clear' для очистки истории")
     print("-" * 40)
@@ -62,7 +69,12 @@ def main():
     models = [{k: d[k] for k in ['name', 'modified_at']} for d in ollama.get('models',[])]
     print(f"Доступные модели: {models}")
     
-    conversation_history = []
+    conversation_history = [
+        {
+            'role': 'system', 
+            'content': SYSTEM_PROMPT
+        }
+    ]
    
     while True:
         try:
@@ -72,6 +84,10 @@ def main():
                 break
             if user_input.lower() == '!clear':
                 conversation_history = []
+                conversation_history.append({
+                    'role': 'system', 
+                    'content': SYSTEM_PROMPT
+                })
                 print("Начат новый диалог")
                 continue
             if not user_input:
@@ -79,11 +95,19 @@ def main():
             conversation_history.append({"role": "user", "content": user_input})
             if len(conversation_history) > MAX_MESSAGES * 2:
                 conversation_history = conversation_history[-MAX_MESSAGES * 2:]
-            response = get_llm_response(user_input, conversation_history)
+            response = get_llm_response_requests(user_input, conversation_history)
             conversation_history.append({"role": "assistant", "content": response})
-            print(f"Бот: {response}")
+            print("=" * 40)
+            print(f"Бот json: {response}")
+            print("\n")
+            response = json.loads(response)
+            if (response.get('error',False)):
+                print(f"Ошибка: {response.get('error')}")
+            else:
+                print(f"Бот: \nОтвет - {response.get('answer','')}\n Пример кода - {response.get('code','')}\nПояснения - {response.get('note','')}")
         except Exception as e:
             print(f"Произошла ошибка: {e}")
+            print(response)
 
 if __name__ == "__main__":
     main()
